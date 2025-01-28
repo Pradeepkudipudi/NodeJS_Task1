8bed85f1ab845a96657c1b4bf3b7ece91b44e0ee
Pradeepkudipudi <pradeepk70220@gmail.com>
2025-01-28 12:19:54 +0530

    add all

---
+++ index.js
@@ -0,0 +1,126 @@
+// create 3 files using writeFile() 
+// Index122.js 
+// Index23r.js 
+// Index24r.js 
+// We have to use writeFile() once 
+
+// And delete the above files using unlink() method. We have to use the method only once and delete all the files
+
+
+
+
+
+
+let fs = require("fs")
+
+
+// creation 
+
+
+// let arr = ["Index122.js","Index23r.js","Index24r.js"]
+
+// arr.forEach((arr)=>{
+// fs.writeFile(`${arr}`,`"i am pradeep"`,"utf-8",(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else{
+//         console.log("file created succesfully");
+        
+//     }
+// })
+// })
+
+
+//  deletion
+
+fs.unlink("./Index24r.js",(err)=>{
+    if(err){
+        console.log(err);
+        
+    }else{
+        console.log("deleted");
+        
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// trail 1
+
+
+
+// let fs = require("fs")
+
+// let arr = ["Index122.js","Index23r.js","Index24r.js"]
+
+// arr.forEach((arr)=>{
+
+//     fs.writeFile(`./${arr}`,"i am pradeep","utf-8",(err)=>{
+
+//     if(err){
+//         console.log(err);
+        
+//     }else{
+//         console.log("file created");
+        
+//     }
+// })
+
+
+// })
+
+
+
+// fs.unlink("./Index24r.js",(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else{
+//         console.log("deleted");
+        
+//     }
+// })
+
